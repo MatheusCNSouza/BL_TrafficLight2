b6ff546558f4a99f3d67ea3d29c2741b8bb73b5f
Matheus Carvalho Nascimento de Souza <45440149+MatheusCNSouza@users.noreply.github.com>
2020-11-15 00:32:35 -0300

    base1

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,31 @@ typedef enum{state_1, state_2, state_3, state_A, state_O} State;
 State init = state_1;
 float led_aux = 1.0f;
   
-void estado_1{
+void estado_1(){
   red_led = 1;
   yellow_led = 0;
   green_led = 0;
 }
 
-void estado_2{
+void estado_2(){
   red_led = 0;
   yellow_led = 0;
   green_led = 1;
 }
 
-void estado_3{
+void estado_3(){
   red_led = 0;
   yellow_led = 1;
   green_led = 0;
 }
 
-void estado_A{
+void estado_A(){
   red_led = 0;
   yellow_led = 0;
   green_led = 0;
 }
 
-void estado_O{
+void estado_O(){
   red_led = 1;
   yellow_led = 0;
   green_led = 0;
