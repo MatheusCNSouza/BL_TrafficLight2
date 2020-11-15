4d9180748e93dc5e7deefd9d1c9dd9a70848fa0b
Matheus Carvalho Nascimento de Souza <45440149+MatheusCNSouza@users.noreply.github.com>
2020-11-14 22:36:11 -0300

    base

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
 #include <mbed.h>
 
-int main() {
+DigitalOut red_led(p5);
+DigitalOut yellow_led(p6);
+DigitalOut green_led(p7);
+InterruptIn btn(p8);
+Ticker ticker;
+typedef enum{state_1, state_2, state_3, state_A, state_O} State;
+State init = state_1;
+float led_aux = 1.0f;
+  
+void estado_1{
+  red_led = 1;
+  yellow_led = 0;
+  green_led = 0;
+}
 
-  // put your setup code here, to run once:
+void estado_2{
+  red_led = 0;
+  yellow_led = 0;
+  green_led = 1;
+}
+
+void estado_3{
+  red_led = 0;
+  yellow_led = 1;
+  green_led = 0;
+}
+
+void estado_A{
+  red_led = 0;
+  yellow_led = 0;
+  green_led = 0;
+}
+
+void estado_O{
+  red_led = 1;
+  yellow_led = 0;
+  green_led = 0;
+}
+
+int main() {
+  estado_1();
 
   while(1) {
     // put your main code here, to run repeatedly:
