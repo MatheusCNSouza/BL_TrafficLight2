e9599708922ebbe01948b7d9b14393ef15c8546f
Matheus Carvalho Nascimento de Souza <45440149+MatheusCNSouza@users.noreply.github.com>
2020-11-15 15:44:46 -0300

    states base

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,13 @@ Ticker ticker;
 typedef enum{state_1, state_2, state_3, state_A, state_O} State;
 State init = state_1;
 float led_aux = 1.0f;
-  
+
+void ticker_a(){
+  static int a_aux = 1;
+  a_aux = !a_aux;
+  yellow_led = a_aux? led_aux:0;
+}
+
 void estado_1(){
   red_led = 1;
   yellow_led = 0;
@@ -23,8 +29,9 @@ void estado_2(){
 
 void estado_3(){
   red_led = 0;
-  yellow_led = 1;
+  yellow_led = led_aux;
   green_led = 0;
+  ticker.attach(ticker_a,1.0);
 }
 
 void estado_A(){
@@ -34,14 +41,14 @@ void estado_A(){
 }
 
 void estado_O(){
-  red_led = 1;
+  red_led = 0;
   yellow_led = 0;
   green_led = 0;
 }
 
 int main() {
   estado_1();
-
+  btn.rise(&btCall)
   while(1) {
     // put your main code here, to run repeatedly:
   }
