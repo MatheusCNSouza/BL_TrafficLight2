424d57ea32ec81ba029f4c2276cef32b8295f0f1
Matheus Carvalho Nascimento de Souza <45440149+MatheusCNSouza@users.noreply.github.com>
2020-11-15 19:54:37 -0300

    add partial count

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,16 @@ DigitalOut green_led(p7);
 InterruptIn btn(p8);
 Ticker ticker;
 typedef enum{state_1, state_2, state_3, state_A, state_O} State;
-State init = state_1;
+float times[] = {10.0, 20.0, 4.0};
+int state = state_1;
 float led_aux = 1.0f;
+Timeout timeout;
+int tbt = 0;
+int prev;
+
+// void btCall(){
+
+// }
 
 void ticker_a(){
   static int a_aux = 1;
@@ -16,24 +24,29 @@ void ticker_a(){
 }
 
 void estado_1(){
+  state = state_1;
   red_led = 1;
   yellow_led = 0;
   green_led = 0;
 }
 
 void estado_2(){
+  state = state_2;
   red_led = 0;
   yellow_led = 0;
   green_led = 1;
 }
 
 void estado_3(){
+  state = state_3;
   red_led = 0;
   yellow_led = 1;
   green_led = 0;
 }
 
-void estado_A(){
+void estado_A(int pre){
+  prev = pre;
+  state = state_A;
   red_led = 0;
   yellow_led = led_aux;
   green_led = 0;
@@ -41,15 +54,62 @@ void estado_A(){
 }
 
 void estado_O(){
+  state = state_O;
   red_led = 0;
   yellow_led = 0;
   green_led = 0;
 }
 
+void time_transition(){
+  if (state < 2){
+    state++;
+    int st = state;
+    switch (st){ 
+    case state_1:
+      estado_1();
+      break;
+    case state_2:
+      estado_2();
+      break;
+    default:
+      break;
+    }
+  }else{
+    state = 0;
+    estado_1();
+    }
+  }
+
+  void btn_transition(){
+    if (tbt > 10){
+      if (state == state_O) estado_1();
+      else estado_O();
+      } else if (state == state_1){
+        if (tbt == 10) estado_2();
+        else if (tbt >= 3) estado_A(0);
+      } else if (state == state_2 ){
+        if (tbt >= 3) estado_A(1);
+        else estado_3();
+      } else if (state == state_3 ){
+        if (tbt >= 3) estado_A(2);      
+      } else if (state == state_A ){
+        if (prev == )
+    }
+    tbt = 0;
+  }
+
+  void btn_counter(){
+    tbt++;
+    wait_ms(1000);
+    printf("count up");
+  }
+
 int main() {
   estado_1();
-  btn.rise(&btCall)
+  btn.fall(&btn_counter);
+  btn.rise(&btn_transition);
+  timeout.attach (&time_transition, times[state]);
   while(1) {
-    // put your main code here, to run repeatedly:
+    wait_ms(10);
   }
 }
